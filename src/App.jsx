52370e5a7f6695b1f2663acd5307b39216deb853
Cassidy <cassidymcmichael617@gmail.com>
2023-07-23 19:29:07 -0400

    update: added validation for input when empty and filled. Also made changes to page styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,27 @@ import './App.css'
 import { DesktopDesign } from "./components/desktop"
 
 function App() {
-  let email = document.getElementById('email').value;
-
   const validateInput = (event) => {
-    if (email !== "") {
-      document.getElementById("email").style.outline
+    event.preventDefault();
+    let email = document.getElementById('email').value;
+    let emailPattern = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$');
+
+    if (email === "") {
+      document.getElementById("email").style.outline = "1px solid hsl(0, 100%, 67%)";
+      document.getElementById("email").style.backgroundColor = "hsla(4, 100%, 80%, 0.26)";
+      document.getElementById("email").style.color = "hsl(0, 100%, 67%)";
+      document.getElementById('error-text').style.display = "none"
+    } else if (!emailPattern.test(email)) {
+      document.getElementById('error-text').style.display = "inline-block"
+      document.getElementById('error-text').style.color = "hsl(0, 100%, 67%)"
+      console.log("NOT a match!");
+    } else {
+      document.getElementById("email").style.outline = "1px solid hsl(0, 0%, 83%)";
+      document.getElementById("email").style.backgroundColor = "hsla(4, 100%, 80%, 0)";
+      document.getElementById("email").style.color = "hsl(0, 0%, 30%)";
+      document.getElementById('error-text').style.display = "none"
+      console.log(`input was NOT empty~`);
+      console.log(`input matches regexpattern~`);
     }
   }
 
@@ -20,25 +36,38 @@ function App() {
           <p className='heading'>Stay updated!</p>
           <p className='body-text'>
             Join the 60,000+ product managers receiving monthly updates on:
-
-            <ul>
-              <li>Product discovery and building what matters</li>
-              <li>Measuring to ensure updates are a success</li>
-              <li>And much more!</li>
-            </ul>
-
           </p>
 
-
-          <div className='inputs'>
-            <div>
-              <label htmlFor='email'>Email address</label><br />
-              <input type="email" placeholder='email@company.com' id="email" />
+          <div>
+            <div className='list'>
+              <img src="../assets/images/icon-list.svg" className='icon-list' />
+              <p>Product discovery and building what matters</p>
             </div>
-            <div>
-              <button type='button'>Subscribe to monthly newsletter</button>
+            <div className='list'>
+              <img src="../assets/images/icon-list.svg" className='icon-list' />
+              <p>Measuring to ensure updates are a success</p>
+            </div>
+
+            <div className='list'>
+              <img src="../assets/images/icon-list.svg" className='icon-list' />
+              <p>And much more!</p>
             </div>
           </div>
+          
+          <form className='form' id="form" name="email-input" action="" method="" autoComplete='off'>
+            <div className='inputs'>
+              <div>
+                <div className='input-label'>
+                  <div className='label-left'><label htmlFor='email'>Email address</label><br /></div>
+                  <div className='label-right'><label id='error-text' className='error-text' htmlFor='email'>Valid email required</label></div>
+                </div>
+                <input type="email" placeholder='email@company.com' id="email" />
+              </div>
+              <div>
+                <button type='button' onClick={(e) => { validateInput(e); }}>Subscribe to monthly newsletter</button>
+              </div>
+            </div>
+          </form>
 
         </div>
         <div className='img'>
